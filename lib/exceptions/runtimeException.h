e14f03eba814319b563a534441270d36a11dce1e
Michael Brookes <mbircohoakeels@me.com>
2017-03-28 22:33:43 +0100

    A new exception class for handling runtime exceptions

--- lib/exceptions/runtimeException.h
+++ lib/exceptions/runtimeException.h
@@ -1,8 +1,51 @@
-//
-// Created by Michael Brookes on 26/03/2017.
-//
+/*
+Copyright (C) 2017 Michael Brookes
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef QUADRO_RUNTIMEEXCEPTIONS_H
 #define QUADRO_RUNTIMEEXCEPTIONS_H
+
+#include <cstdio>
+#include <exception>
+#include <string>
+
+namespace quadro {
+
+    namespace exceptions {
+
+        using namespace std;
+
+        /**
+         * TODO: add logging.
+         */
+        class runtimeException : public exception {
+        public:
+
+            runtimeException() { }
+
+            runtimeException( string errMessage )
+                    :errMessage_( errMessage ) { }
+
+            const char* what() const throw() { return errMessage_.c_str(); }
+
+            string errMessage_;
+        };
+
+    }
+
+}
 
 #endif //QUADRO_RUNTIMEEXCEPTIONS_H
