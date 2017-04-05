16bc60e24065af2d7be24537ed27ed8d73489140
Michael Brookes <mbircohoakeels@me.com>
2017-04-05 20:57:14 +0100

    Seperated start function out from constructor to a public function - improved comments

--- lib/i2c/devices/L3GD20H/L3GD20H.cpp
+++ lib/i2c/devices/L3GD20H/L3GD20H.cpp
@@ -1,88 +1,119 @@
+/*
+Copyright (C) 2017 Michael Brookes
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "L3GD20H.h"
 
 using namespace quadro::i2c;
 
-/**
- \brief BBBLSM303DLHC : A class that provides control of the accelerometer LSM303DLHC and extends I2C devices on the Beaglebone Black.
-  BBBI2CDevice : A low level control of I2C devices on the Beaglebone Black.
- */
 L3GD20H::L3GD20H( unsigned char _deviceAddress, int _busId )
 {
-    this->setDeviceAddress( _deviceAddress );
-    this->setBusId( _busId );
-    this->initDevice();
+    setDeviceAddress( _deviceAddress );
+    setBusId( _busId );
+    initDevice();
 
-    if ( this->setPower( _switch::ON ) < 0 ) {
-        cerr << "Critical Error 1 : LSM303DLHC - Attempted to set the Accelerometers Power state to : "
-             << this->gyroSwitch << " | This action failed." << endl;
-        exit( 1 );
+    if ( setPower( _switch::ON ) < 0 ) {
+        throw new i2cSetupException( "Critical Error : L3GD20H - Attempted to set the Gyroscopes Power state but this action failed." );
     }
-
-    this->start();
 }
 
 int L3GD20H::setPower( _switch gyroSwitch )
 {
-    this->gyroSwitch = gyroSwitch;
-    this->setRegisterAddress( CTRL1 );
-    this->setRegisterValue( gyroSwitch );
-    this->writeToDevice( 2 );
+    setRegisterAddress( CTRL1 );
+    setRegisterValue( gyroSwitch );
+    writeToDevice( 2 );
     return gyroSwitch;
 }
 
-void* L3GD20H::getValues( void* gyroInst )
+void* L3GD20H::runMainSensorUpdateThread( void* static_inst )
 {
-    L3GD20H* gyro = ( L3GD20H* ) gyroInst;
+    L3GD20H* gyro = ( L3GD20H* ) static_inst;
     int startTime;
     while ( 1 ) {
         startTime = Timer::milliTimer();
-        gyro->setX();
-        gyro->setY();
-        gyro->setZ();
+        gyro->calcX();
+        gyro->calcY();
+        gyro->calcZ();
         while ( Timer::milliTimer() - startTime < ( DT * 1000 )) {
             usleep( 100 );
         }
     }
 }
 
-void L3GD20H::start()
+int L3GD20H::start()
 {
-    pthread_create( &this->gyroNotifyer, NULL, L3GD20H::getValues, this );
+    //pthread_create doesn't throw an exception, only returns error codes - these are handled below.
+    threadRet = pthread_create( &threadHandle, NULL, L3GD20H::runMainSensorUpdateThread, this );
+
+    //Thread returned 0 (Success Code)
+    if ( threadRet != 0 ) {
+        //The sonic sensor thread failed, set the status to "Error" and throw an appropriate exception.
+        setStatus( deviceStatus::Error );
+        if ( threadRet == EAGAIN ) {
+            //Failed because of resource unavailability, try once more and then throw an exception on failure
+            threadRet = pthread_create( &threadHandle, NULL, L3GD20H::runMainSensorUpdateThread, this );
+            if ( threadRet != 0 ) {
+                throw new i2cSetupException(
+                        "(LVMaxSonarEZ) " + i2c::THREAD_FATAL + " : errorNumber = "
+                                + to_string( threadRet ) );
+            }
+        }
+        else if ( threadRet == EPERM ) {
+            //Thread creation failed because of invalid permissions on the system to create threads.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_PERMISSIONS + " : errorNumber = " + to_string( threadRet ) );
+        }
+        else if ( threadRet == EINVAL ) {
+            //Thread creation failed because the argument used is invalid.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_INVALID_ARG + " : errorNumber = " + to_string( threadRet ) );
+        }
+        else {
+            //An unknown error occurred - unknown error code.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_UNKNOWN + " : errorNumber = " + to_string( threadRet ) );
+        }
+    }
+
+    //The thread started correctly, so set the status of the Sonic Sensor to "On".
+    setStatus( deviceStatus::On );
+
+    //return the device status - realistically it should always be "On" if we get this far...
+    return getStatus();
+
 }
 
-/*!
- @function SetX
- - Set this->g.X to the current X reading from the sensor
- @returns <void>
- */
-void L3GD20H::setX( void )
+void L3GD20H::calcX( void )
 {
-    this->rad.x = ( int16_t ) ( this->getValueFromRegister( OUT_X_L ) | ( this->getValueFromRegister( OUT_X_H ) << 8 ));
-    this->rad.x *= 0.00875F;
-    this->angle.x = float( this->rad.x * DT );
+    rad.x = ( int16_t ) ( getValueFromRegister( OUT_X_L ) | ( getValueFromRegister( OUT_X_H ) << 8 ));
+    rad.x *= 0.00875F;
+    angle.x = float( rad.x * DT );
 }
 
-/*!
- @function SetY
- - Set this->g.Y to the current Y reading from the sensor
- @returns <void>
- */
-void L3GD20H::setY( void )
+void L3GD20H::calcY( void )
 {
-    this->rad.y = ( int16_t ) ( this->getValueFromRegister( OUT_Y_L ) | ( this->getValueFromRegister( OUT_Y_H ) << 8 ));
-    this->rad.y *= 0.00875F;
-    this->angle.y = float( this->rad.y * DT );
+    rad.y = ( int16_t ) ( getValueFromRegister( OUT_Y_L ) | ( getValueFromRegister( OUT_Y_H ) << 8 ));
+    rad.y *= 0.00875F;
+    angle.y = float( rad.y * DT );
 }
 
-/*!
- @function SetZ
- - Set this->g.Z to the current Z reading from the sensor
- @returns <void>
- */
-void L3GD20H::setZ( void )
+void L3GD20H::calcZ( void )
 {
-    this->rad.z = -( int16_t ) ( this->getValueFromRegister( OUT_Z_L )
-            | ( this->getValueFromRegister( OUT_Z_H ) << 8 ));
-    this->rad.z *= 0.00875F;
-    this->angle.z = float( this->rad.z * DT );
+    rad.z = -( int16_t ) ( getValueFromRegister( OUT_Z_L )
+            | ( getValueFromRegister( OUT_Z_H ) << 8 ));
+    rad.z *= 0.00875F;
+    angle.z = float( rad.z * DT );
 }
