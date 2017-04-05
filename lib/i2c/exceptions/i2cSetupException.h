4f3d4e076f0399a21371780908ab42e011fd5a67
Michael Brookes <mbircohoakeels@me.com>
2017-04-05 21:23:14 +0100

    Error messages added as const string values

--- lib/i2c/exceptions/i2cSetupException.h
+++ lib/i2c/exceptions/i2cSetupException.h
@@ -15,6 +15,12 @@ namespace quadro {
 
         using namespace std;
 
+        const string STARTUP_FAILURE = "Unable to start the I2C device.";
+        const string THREAD_UNKNOWN = "Unable to create thread : Unknown Error Occurred.";
+        const string THREAD_FATAL = "Unable to create thread : Fatal Error Occurred.";
+        const string THREAD_PERMISSIONS = "Unable to create thread : Operation not permitted.";
+        const string THREAD_INVALID_ARG = "Unable to create thread : Invalid Argument.";
+
         class i2cSetupException : public setupException {
         public:
             /**
