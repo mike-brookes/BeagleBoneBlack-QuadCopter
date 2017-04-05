21daf49efb14aea8d281d397786f23bdf39a7e4e
Michael Brookes <mbircohoakeels@me.com>
2017-04-05 21:21:09 +0100

    Improved start, stop methods for handling the update thread, improved comments

--- lib/i2c/devices/LSM303/Magnetometer/LSM303Magnetometer.cpp
+++ lib/i2c/devices/LSM303/Magnetometer/LSM303Magnetometer.cpp
@@ -21,23 +21,22 @@ using namespace quadro::i2c;
 
 LSM303Magnetometer::LSM303Magnetometer( unsigned char _deviceAddress, int _busId )
 {
-    this->setDeviceAddress( _deviceAddress );
-    this->setBusId( _busId );
-    this->initDevice();
-    this->loadRecommendedFlightSettings();
-    this->init();
-    this->startRecordingHeading();
+    setDeviceAddress( _deviceAddress );
+    setBusId( _busId );
+    initDevice();
+    loadRecommendedFlightSettings();
+    startSensorThread();
 }
 
-double LSM303Magnetometer::heading()
+double LSM303Magnetometer::calcHeading()
 {
     double x2 = 0, y2 = 0;
 
-    double t_roll = this->x * this->x + this->z * this->z;
-    float rollRadians = ( float ) atan2(( double ) this->y, sqrt( t_roll ));
+    double t_roll = x * x + z * z;
+    float rollRadians = ( float ) atan2(( double ) y, sqrt( t_roll ));
 
-    double t_pitch = this->y * this->y + this->z * this->z;
-    float pitchRadians = ( float ) atan2(( double ) this->x, sqrt( t_pitch ));
+    double t_pitch = y * y + z * z;
+    float pitchRadians = ( float ) atan2(( double ) x, sqrt( t_pitch ));
 
     float cosRoll = ( float ) cos( rollRadians );
     float sinRoll = ( float ) sin( rollRadians );
@@ -45,42 +44,61 @@ double LSM303Magnetometer::heading()
     float cosPitch = ( float ) cos( -1 * pitchRadians );
     float sinPitch = ( float ) sin( -1 * pitchRadians );
 
-    x2 = ( this->x ) * cosPitch + ( this->z ) * sinPitch;
-    y2 = ( this->x ) * sinRoll * sinPitch + ( this->y ) * cosRoll - ( this->z ) * sinRoll * cosPitch;
+    x2 = ( x ) * cosPitch + ( z ) * sinPitch;
+    y2 = ( x ) * sinRoll * sinPitch + ( y ) * cosRoll - ( z ) * sinRoll * cosPitch;
 
     return ( atan2( x2, y2 ) * 180 ) / M_PI;
 }
 
-void LSM303Magnetometer::setHeadingAverages( int iterations )
+void* LSM303Magnetometer::runMainSensorUpdateThread( void* static_inst )
 {
-    int i = 0, _totalHeading = 0;
-    while ( i < iterations ) {
-        _totalHeading += this->heading();
-        usleep( this->dataTimer );
-        i++;
+    LSM303Magnetometer* mag = ( LSM303Magnetometer* ) static_inst;
+    while ( mag->currentStatus == deviceStatus::On ) {
+        mag->heading = mag->calcHeading();
+        usleep( mag->dataTimer );
     }
-    this->avgHeading = _totalHeading / iterations;
+    return 0;
 }
 
-void LSM303Magnetometer::addToHeadingDataStore()
+int LSM303Magnetometer::start()
 {
-    for ( int i = 0; i < ( MAX_DATASTORE - 1 ); i++ ) {
-        int NextIndex = ( i + 1 );
-        this->headingDataStoredValues[ i ] = this->headingDataStoredValues[ NextIndex ];
-    }
-    this->headingDataStoredValues[ ( MAX_DATASTORE - 1 ) ] = this->heading();
-}
+    //pthread_create doesn't throw an exception, only returns error codes - these are handled below.
+    threadRet = pthread_create( &threadHandle, NULL, LSM303Magnetometer::runMainSensorUpdateThread, this );
 
-void LSM303Magnetometer::startRecordingHeading()
-{
-    pthread_create( &this->LSM303UpdateValuesThread, NULL, LSM303Magnetometer::getMagnetometerValues, this );
-}
-
-void* LSM303Magnetometer::getMagnetometerValues( void* _ACCEL )
-{
-    LSM303Magnetometer* accel = ( LSM303Magnetometer* ) _ACCEL;
-    while ( 1 ) {
-        accel->addToHeadingDataStore();
-        usleep( accel->dataTimer );
+    //Thread returned 0 (Success Code)
+    if ( threadRet != 0 ) {
+        //The sonic sensor thread failed, set the status to "Error" and throw an appropriate exception.
+        setStatus( deviceStatus::Error );
+        if ( threadRet == EAGAIN ) {
+            //Failed because of resource unavailability, try once more and then throw an exception on failure
+            threadRet = pthread_create( &threadHandle, NULL, LSM303Magnetometer::runMainSensorUpdateThread, this );
+            if ( threadRet != 0 ) {
+                throw new i2cSetupException(
+                        "(LVMaxSonarEZ) " + i2c::THREAD_FATAL + " : errorNumber = "
+                                + to_string( threadRet ) );
+            }
+        }
+        else if ( threadRet == EPERM ) {
+            //Thread creation failed because of invalid permissions on the system to create threads.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_PERMISSIONS + " : errorNumber = " + to_string( threadRet ) );
+        }
+        else if ( threadRet == EINVAL ) {
+            //Thread creation failed because the argument used is invalid.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_INVALID_ARG + " : errorNumber = " + to_string( threadRet ) );
+        }
+        else {
+            //An unknown error occurred - unknown error code.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_UNKNOWN + " : errorNumber = " + to_string( threadRet ) );
+        }
     }
+
+    //The thread started correctly, so set the status of the Sonic Sensor to "On".
+    setStatus( deviceStatus::On );
+
+    //return the device status - realistically it should always be "On" if we get this far...
+    return getStatus();
+
 }
--- lib/i2c/devices/LSM303/Magnetometer/LSM303Magnetometer.h
+++ lib/i2c/devices/LSM303/Magnetometer/LSM303Magnetometer.h
@@ -24,8 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using namespace std;
 
-#define MAX_DATASTORE 15
-
 namespace quadro {
 
     namespace i2c {
@@ -34,25 +32,53 @@ namespace quadro {
 
         public:
 
+            /*!
+             * LSM303Magnetometer Constructor, initialises the device - writes default settings to the sensor
+             *
+             * @see start() to start reading the device and updating heading values.
+             * @param _deviceAddress
+             * @param _busId
+             */
             LSM303Magnetometer( unsigned char _deviceAddress = MAG_ADDRESS, int _busId = 1 );
 
-            void setHeadingAverages( int iterations = 50 );
+            /*!
+             * Calculates heading using the following equation :
+             *
+             * x2 = ( x ) * cos( -1 * atan2( X, sqrt( Y * Y + Z * Z )) ) + ( z ) * sin( -1 * atan2( X, sqrt( Y * Y + Z * Z )) );
+             * y2 = ( x ) * sin( atan2( Y, sqrt( X * X + Z * Z )) ) * sin( -1 * atan2( X, sqrt( Y * Y + Z * Z )) ) + ( y ) * cos( atan2( Y, sqrt( X * X + Z * Z )) ) - ( z ) * sin( atan2( Y, sqrt( X * X + Z * Z )) ) * cos( -1 * atan2( X, sqrt( Y * Y + Z * Z )) );
+             *
+             * return ( atan2( x2, y2 ) * 180 ) / 3.14159265358979323846;
+             *
+             * @return double Current heading calculation
+             */
+            double calcHeading();
 
-            double heading();
+            /**
+             * start() - Allows the independent control to start the thread that updates the Magnetometer readings.
+             *
+             * @param none
+             * @throws i2cSetupException
+             * @returns int deviceStatus : 1 = on, 0 = off or 2 = error
+             */
+            int start();
 
-            double avgHeading;
+            double heading; //!< Store current calculated heading value;
 
-            double headingDataStoredValues[MAX_DATASTORE];
+            pthread_t threadHandle; //!< @var Thread Handle
 
         private:
 
-            void startRecordingHeading();
+            /*!
+             * runMainSensorUpdateThread starts a continuous thread updating the programs values from the sensors values.
+             *
+             * @see start() to start this thread running
+             * @see stop() to stop this thread running
+             * @param static_inst
+             * @return 0
+             */
+            static void* runMainSensorUpdateThread( void* static_inst );
 
-            void addToHeadingDataStore();
-
-            static void* getMagnetometerValues( void* _ACCEL );
-
-            pthread_t LSM303UpdateValuesThread;
+            int threadRet; //!< @var thread return value
 
         };
     }
