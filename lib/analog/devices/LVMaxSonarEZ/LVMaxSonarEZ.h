fc6bf785cf5d8694c2a8cb906c8aefa40241c91d
Michael Brookes <mbircohoakeels@me.com>
2017-04-05 20:53:46 +0100

    Moved defines to const values - improved comments

--- lib/analog/devices/LVMaxSonarEZ/LVMaxSonarEZ.h
+++ lib/analog/devices/LVMaxSonarEZ/LVMaxSonarEZ.h
@@ -21,22 +21,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../../analogBase.h"
 #include <sstream>
 
-#define MAX_OPERATING_HEIGHT_IN_INCES   255
-#define MIN_OPERATING_HEIGHT_IN_INCES   6
-
-#define EMERGENCY_HEIGHT_CM             23
-#define EMERGENCY_HEIGHT_INCH           9
-
-#define SONIC_INCH_MODIFYER             9.8
-#define SONIC_CM_MODIFYER               2.54
-#define SONIC_DATATIMER                 50000 //20HZ
-
 namespace quadro {
 
     namespace analog {
 
         class LVMaxSonarEZ : public analogBase {
+
         public:
+
+            const float SONIC_INCH_MODIFIER = 9.8f; //!< @var Used in calculating the current height in Inches
+            const float SONIC_CM_MODIFIER = 2.54f; //!< @var Used in calculating the current height in Centimetres
+
+            static const unsigned int SONIC_DATATIMER = 50000; //!< @var Used to time each iteration of the thread gathering sensor data (20hz)
+
+            const int MAX_OPERATING_HEIGHT_IN_INCES = 255; //!< @var The furthest distance that can be read.
+            const int MIN_OPERATING_HEIGHT_IN_INCES = 6; //!< @var The closest distance that can be read.
 
             /**
              * Class constructor - by default this constructor sets the height mode to CM and the devices status to off.
@@ -58,45 +57,84 @@ namespace quadro {
              * @throws analogRuntimeException
              * @returns enum deviceStatus : 1 = on, 0 = off or 2 = error
              */
-            void stop();
+            int stop();
 
-            double distance();
+            /**
+             * This method provides access to the private calculate distance result.
+             *
+             * @return double
+             */
+            double getDistance();
 
+            //!< Storage of the timer for the sensors main thread, based on the data rate of the sensor
             __useconds_t dataTimer = SONIC_DATATIMER;
 
+            /*!
+             * Select a deviceMode, changes the returned distance value to the selected type.
+             */
             enum deviceMode {
                 Inches = 0,
                 Cm = 1
             };
 
+            /*!
+             * Stores the current deviceStatus for easy reference fro other objects
+             */
             enum deviceStatus {
-                On = 1,
-                Off = 0,
-                Error = 2
+                On = 1, //!< Status On (thread is active)
+                Off = 0, //!< Status Off (thread is disabled)
+                Error = 2 //!< Status Error (thread is disabled)
             };
 
+            //!< @var currentStatus holds a deviceStatus value
             deviceStatus currentStatus;
+
+            //!< @var currentMode holds a deviceMode value
             deviceMode currentMode;
 
+            /*!
+             * setMode allows the mode of the sensor to be changed to one of the values set in deviceModes
+             *
+             * @param _mode
+             */
             void setMode( deviceMode _mode );
 
+            /*!
+             * getStatus returns the current status of the sensor
+             *
+             * @return int
+             */
             int getStatus();
 
+            /*!
+             * setStatus allows the status of the sensor to be changed to one of the values set in deviceStatus
+             *
+             * @param _status
+             */
             void setStatus( deviceStatus _status );
+
+            pthread_t threadHandle; //!< @var pthread_t handle
 
         private:
 
-            static void* getValueAsInt( void* static_inst );
+            /*!
+             * Used in the pthread_create call, starts the thread loop for updating sensor values.
+             *
+             * @param static_inst
+             */
+            static void* runMainSensorUpdateThread( void* static_inst );
 
-            int getReading();
-
+            /*!
+             * This method does the math to convert the sensor reading to a distance stored as a double
+             *
+             * @param _reading
+             * @return double
+             */
             double convertReadingToDistance( int _reading );
 
-            int reading;
+            int reading; //!< @var contains the current sensor raw value, updated in the thread loop
 
-            pthread_t sonicReaderThread;
-
-            int threadRet;
+            int threadRet; //!< @var thread return value
         };
 
     }
