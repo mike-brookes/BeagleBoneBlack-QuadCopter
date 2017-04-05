b9abfc016b3bea9b477732e8d394106adde34ec4
Michael Brookes <mbircohoakeels@me.com>
2017-04-05 21:29:08 +0100

    Improved error handling, added start() method calls for all sensors, improved comments

--- lib/orientation/orientation.cpp
+++ lib/orientation/orientation.cpp
@@ -34,21 +34,47 @@ orientation::orientation( )
         //Create a new LSM303Accelerometer, this sensors thread is initiated in the constructor
         accelerometer = new LSM303Accelerometer();
 
+        //Start the sensors main thread, runs while the sensor is on.
+        accelerometer->start();
+
         //Create a new LSM303Magnetometer, this sensors thread is initiated in the constructor
         magnetometer = new LSM303Magnetometer();
 
+        //Start the sensors main thread, runs while the sensor is on.
+        magnetometer->start();
+
         //Create a new L3GD20H, this sensors thread is initiated in the constructor
         gyroscope = new L3GD20H();
+
+        //Start the sensors main thread, runs while the sensor is on.
+        gyroscope->start();
     }
     catch( analog::analogSetupException& e ){
         throw new setupException( analog::STARTUP_FAILURE + " Details : " + e.what() );
     }
+    catch( i2c::i2cSetupException& e ){
+        throw new setupException( i2c::STARTUP_FAILURE + " Details : " + e.what() );
+    }
+
+    //Set up a new PID object for Pitch
+    pitchPID = new PID( 0.2, 100, -100, 0.1, 0.01, 0.5 );
+
+    //Set up a new PID object for Roll
+    rollPID = new PID( 0.2, 100, -100, 0.1, 0.01, 0.5 );
 
     //select the Kalman filter method by default, this can be altered using the public setDataFilterSelection method.
     setDataFilterSelection( KALMAN );
 
     //Start the orientation main thread that calculates it's own values based on sensor thread updated values.
     pthread_create( &orientationNotifyer, NULL, orientation::setValues, this );
+}
+
+orientation::~orientation() noexcept
+{
+    sonicSensor->stop( );
+    accelerometer->stop( accelerometer->threadHandle );
+    magnetometer->stop( magnetometer->threadHandle );
+    gyroscope->stop( gyroscope->threadHandle );
 }
 
 void* orientation::setValues( void* orientationInst )
@@ -72,23 +98,17 @@ void* orientation::setValues( void* orientationInst )
             position->runKalmanCalculations();
         }
 
-        while ( Timer::milliTimer() - startTime < ( DATA_RATE * 1000 )) {
+        while ( Timer::milliTimer() - startTime < ( position->DATA_RATE * 1000 )) {
             usleep( 100 );
         }
 
     }
 }
 
-int orientation::getEmergencyHeight()
-{
-    return ( sonicSensor->currentMode == sonicSensor->deviceMode::Cm ) ? EMERGENCY_HEIGHT_CM
-                                                                       : EMERGENCY_HEIGHT_INCH;
-}
-
 void orientation::runKalmanCalculations()
 {
-    pitch = float( kalmanPitch.getAngle( accelerometer->pitch(), gyroscope->angle.x, quadro::DATA_RATE ) );
-    roll = float( kalmanRoll.getAngle( accelerometer->roll(), gyroscope->angle.y, quadro::DATA_RATE ) );
+    pitch = float( kalmanPitch.getAngle( accelerometer->pitch, gyroscope->angle.x, DATA_RATE ) );
+    roll = float( kalmanRoll.getAngle( accelerometer->roll, gyroscope->angle.y, DATA_RATE ) );
 
     yaw = 0;
     height = 0;
@@ -101,8 +121,8 @@ void orientation::runComplimentaryCalculations()
 {
     float CFANGLEX = 0;
     float CFANGLEY = 0;
-    pitch = quadro::COMPLIMENTARY_FILTER_TUNING * ( CFANGLEX + gyroscope->angle.x * DATA_RATE ) + ( 1 - quadro::COMPLIMENTARY_FILTER_TUNING ) * accelerometer->pitch();
-    roll = quadro::COMPLIMENTARY_FILTER_TUNING * ( CFANGLEY + gyroscope->angle.y * DATA_RATE ) + ( 1 - quadro::COMPLIMENTARY_FILTER_TUNING ) * accelerometer->roll();
+    pitch = COMPLIMENTARY_FILTER_TUNING * ( CFANGLEX + gyroscope->angle.x * DATA_RATE ) + ( 1 - COMPLIMENTARY_FILTER_TUNING ) * accelerometer->pitch;
+    roll = COMPLIMENTARY_FILTER_TUNING * ( CFANGLEY + gyroscope->angle.y * DATA_RATE ) + ( 1 - COMPLIMENTARY_FILTER_TUNING ) * accelerometer->roll;
 
     yaw = 0;
     height = 0;
--- lib/orientation/orientation.h
+++ lib/orientation/orientation.h
@@ -26,113 +26,108 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../I2C/Devices/LSM303/Accelerometer/LSM303Accelerometer.h"
 #include "../analog/devices/LVMaxSonarEZ/LVMaxSonarEZ.h"
 #include "../I2C/Devices/LSM303/Magnetometer/LSM303Magnetometer.h"
+#include "../external/PID.h"
 
 namespace quadro {
     using namespace analog;
     using namespace i2c;
 
     /**
-    * @var DATA_RATE is the speed that data is being refreshed.
-    */
-    const float DATA_RATE = 0.02;
-
-    /**
-     * @var COMPLIMENTARY_FILTER_TUNING is the percentage of the first value used in the filter, it is offset by
-     * subtracting it from 1 and using that percentage of the second value used in the filters algorithm.
-     */
-    const float COMPLIMENTARY_FILTER_TUNING = 0.98;
-
-    /*!
      * @package quadro::orientation
      */
     class orientation {
 
     public:
 
-        /*!
+        /**
+        * DATA_RATE is the speed that data is being refreshed.
+        */
+        const float DATA_RATE = 0.02;
+
+        /**
+         * COMPLIMENTARY_FILTER_TUNING is the percentage of the first value used in the filter, it is offset by
+         * subtracting it from 1 and using the remainder percentage in the filters algorithm.
+         */
+        const float COMPLIMENTARY_FILTER_TUNING = 0.98;
+
+        /**
          * Orientation constructor - This class initiates a thread that activates sensors and constantly updates/reads all values.
          * From within this class I should have access to everything orientation related : e.g Accelerometer, Magnetometer, Gyroscope, Barometer and Sonic Sensor.
+         *
          * @params none
          * @throws setupException
-         * @TODO Allow selection of filter type : e.g Kalman, Complimentary.
          */
         orientation( );
 
-        /*!
-         * This method is used for determining urgency of movements, attempting to give some kind of priority to actions.
-         * @return int (a value of cm or inches depending on sensor setting)
+        /**
+         * Destructor : Kills all threads.
          */
-        int getEmergencyHeight();
+        ~orientation() noexcept;
 
-        /*!
+        /**
+         * Select a Data Filter from the selection found in dataFilter
+         *
          * @param int dataFilter type : 1 = Kalman Filter, 2 = Complimentary Filter.
          */
         void setDataFilterSelection( int );
 
-        /*!
-         * @var Converted sensor reading storage as float.
-         */
-        float roll, pitch, yaw, baroHeight, sonicHeight, height, heading;
+        float roll; //!< Filtered roll value stored as a float
+        float pitch; //!< Filtered pitch value stored as a float
+        float yaw; //!< Filtered yaw value stored as a float
+        float baroHeight; //!< Filtered baroHeight value stored as a float
+        float sonicHeight; //!< Filtered sonicHeight value stored as a float
+        float height; //!< Filtered height value stored as a float
+        float heading;//!< Filtered heading value stored as a float
 
-        /*!
-         * @var contains the currently selected data filter algorithm.
+        int dataFilterSelection; //!< Contains the currently selected data filter algorithm.
+
+        /**
+         * select a Filter type
          */
-        int dataFilterSelection;
+        enum dataFilter {
+            KALMAN = 1, //!< Kalman Filter algorithm.
+            COMPLIMENTARY = 2 //!< Complimentary Filter algorithm
+        };
+
+        /**
+         * Pointer for a new PID (Proportional–Integral–Derivative) object for pitch values
+         */
+        PID* pitchPID;
+
+        /**
+         * Pointer for a new PID (Proportional–Integral–Derivative) object for roll values
+         */
+        PID* rollPID;
 
     private:
 
-        enum dataFilter {
-            KALMAN = 1,
-            COMPLIMENTARY = 2
-        };
-
-        /*!
+        /**
          * setValues is a static method that runs in it's own thread assigning values from all assigned orientation sensors.
+         *
          * @params *this - a pointer to this object
          */
         static void* setValues( void* orientationInst );
 
-        /*!
-         * @var orientationNotifyer - Thread notifyer
-         */
-        pthread_t orientationNotifyer;
+        pthread_t orientationNotifyer; //!< Thread Handle
 
-        /*!
-         * @var kalmanPitch - Create a Kalman filter object for pitch
-         */
-        Kalman kalmanPitch;
+        Kalman kalmanPitch; //!< kalmanPitch - Create a Kalman filter object for pitch
 
-        /*!
-         * @var kalmanRoll - Create a Kalman filter object for roll
-         */
-        Kalman kalmanRoll;
+        Kalman kalmanRoll; //!< kalmanRoll - Create a Kalman filter object for roll
 
-        /*!
-         * @var sonicSensor - Create a container for the LVMaxSonarEZ object
-         */
-        LVMaxSonarEZ* sonicSensor;
+        LVMaxSonarEZ* sonicSensor; //!< sonicSensor - Create a container for the LVMaxSonarEZ object
 
-        /*!
-         * @var accelerometer - Create a container for the LSM303Accelerometer object
-         */
-        LSM303Accelerometer* accelerometer;
+        LSM303Accelerometer* accelerometer; //!< accelerometer - Create a container for the LSM303Accelerometer object
 
-        /*!
-         * @var magnetometer - Create a container for the LSM303Magnetometer object
-         */
-        LSM303Magnetometer* magnetometer;
+        LSM303Magnetometer* magnetometer; //!< magnetometer - Create a container for the LSM303Magnetometer object
 
-        /*!
-         * @var gyroscope - Create a container for the L3GD20H object
-         */
-        L3GD20H* gyroscope;
+        L3GD20H* gyroscope; //!< gyroscope - Create a container for the L3GD20H object
 
-        /*!
+        /**
          * runKalmanCalculations run all the sensor data through the Kalman filter and assign orientation values.
          */
         void runKalmanCalculations();
 
-        /*!
+        /**
          * runComplimentaryCalculations run all the sensor data through the Complimentary filter and assign orientation values.
          */
         void runComplimentaryCalculations();
