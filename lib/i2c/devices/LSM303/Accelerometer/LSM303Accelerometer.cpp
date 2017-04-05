8bdc1148e8a58a6d771ab681b1751e10dba2d5b2
Michael Brookes <mbircohoakeels@me.com>
2017-04-05 21:03:02 +0100

    Major refactor - better thread handling, improved comments, start() method seperated out to a public method

--- lib/i2c/devices/LSM303/Accelerometer/LSM303Accelerometer.cpp
+++ lib/i2c/devices/LSM303/Accelerometer/LSM303Accelerometer.cpp
@@ -8,35 +8,74 @@ using namespace quadro::i2c;
 
 LSM303Accelerometer::LSM303Accelerometer( unsigned char _deviceAddress, int _busId )
 {
-    this->setDeviceAddress( _deviceAddress );
-    this->setBusId( _busId );
-    this->initDevice();
-    this->loadRecommendedFlightSettings();
-    this->init();
-    this->startRecordingPitchAndRoll();
+    setDeviceAddress( _deviceAddress );
+    setBusId( _busId );
+    initDevice();
+    loadRecommendedFlightSettings();
+    startSensorThread();
 }
 
-float LSM303Accelerometer::roll()
+float LSM303Accelerometer::calcRoll()
 {
-    return (float) ( ( atan2( ( double ) this->x, sqrt( double( this->y * this->y + this->z * this->z ))) * 180.0 ) / M_PI );
+    return (float) ( ( atan2( ( double ) x, sqrt( double( y * y + z * z ))) * 180.0 ) / M_PI );
 }
 
-float LSM303Accelerometer::pitch()
+float LSM303Accelerometer::calcPitch()
 {
-    return (float) ( ( atan2( ( double ) this->y, ( double ) this->z ) * 180.0 ) / M_PI );
+    return (float) ( ( atan2( ( double ) y, ( double ) z ) * 180.0 ) / M_PI );
 }
 
-void LSM303Accelerometer::startRecordingPitchAndRoll()
+int LSM303Accelerometer::start()
 {
-    pthread_create( &this->updateValuesThread, NULL, LSM303Accelerometer::getAccelerometerValues, this );
+    //pthread_create doesn't throw an exception, only returns error codes - these are handled below.
+    threadRet = pthread_create( &threadHandle, NULL, LSM303Accelerometer::runMainSensorUpdateThread, this );
+
+    //Thread returned 0 (Success Code)
+    if ( threadRet != 0 ) {
+        //The sonic sensor thread failed, set the status to "Error" and throw an appropriate exception.
+        setStatus( deviceStatus::Error );
+        if ( threadRet == EAGAIN ) {
+            //Failed because of resource unavailability, try once more and then throw an exception on failure
+            threadRet = pthread_create( &threadHandle, NULL, LSM303Accelerometer::runMainSensorUpdateThread, this );
+            if ( threadRet != 0 ) {
+                throw new i2cSetupException(
+                        "(LVMaxSonarEZ) " + i2c::THREAD_FATAL + " : errorNumber = "
+                                + to_string( threadRet ) );
+            }
+        }
+        else if ( threadRet == EPERM ) {
+            //Thread creation failed because of invalid permissions on the system to create threads.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_PERMISSIONS + " : errorNumber = " + to_string( threadRet ) );
+        }
+        else if ( threadRet == EINVAL ) {
+            //Thread creation failed because the argument used is invalid.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_INVALID_ARG + " : errorNumber = " + to_string( threadRet ) );
+        }
+        else {
+            //An unknown error occurred - unknown error code.
+            throw new i2cSetupException(
+                    "(LVMaxSonarEZ) " + i2c::THREAD_UNKNOWN + " : errorNumber = " + to_string( threadRet ) );
+        }
+    }
+
+    //The thread started correctly, so set the status of the Sonic Sensor to "On".
+    setStatus( deviceStatus::On );
+
+    //return the device status - realistically it should always be "On" if we get this far...
+    return getStatus();
+
 }
 
-void* LSM303Accelerometer::getAccelerometerValues( void* _ACCEL )
+void* LSM303Accelerometer::runMainSensorUpdateThread( void* static_inst )
 {
-    LSM303Accelerometer* accel = ( LSM303Accelerometer* ) _ACCEL;
-    while ( 1 ) {
-        accel->pitch();
-        accel->roll();
+    LSM303Accelerometer* accel = ( LSM303Accelerometer* ) static_inst;
+    while ( accel->currentStatus == deviceStatus::On ) {
+        accel->pitch = accel->calcPitch();
+        accel->roll = accel->calcRoll();
         usleep( accel->dataTimer );
     }
+
+    return 0;
 }
--- lib/i2c/devices/LSM303/Accelerometer/LSM303Accelerometer.h
+++ lib/i2c/devices/LSM303/Accelerometer/LSM303Accelerometer.h
@@ -21,19 +21,60 @@ namespace quadro {
 
         public:
 
+            /*!
+             * LSM303Accelerometer Constructor, initialises the device - writes default settings to the sensor
+             *
+             * @see start() to start reading the device and updating roll and pitch values.
+             * @param _deviceAddress
+             * @param _busId
+             */
             LSM303Accelerometer( unsigned char _deviceAddress = ACCEL_ADDRESS, int _busId = 1 );
 
-            float roll();
+            /*!
+             * Calculates roll using the following equation :
+             * atan2( X, sqrt( Y * Y + Z * Z )) * 180.0 ) / 3.14159265358979323846 )
+             *
+             * @return float Current roll calculation
+             */
+            float calcRoll();
 
-            float pitch();
+            /*!
+             * Calculates pitch using the following equation :
+             * atan2( Y, Z ) * 180.0 ) / 3.14159265358979323846
+             *
+             * @return float Current pitch calculation.
+             */
+            float calcPitch();
+
+            /**
+             * start() - Allows the independent control to start the thread that updates the Accelerometer readings.
+             *
+             * @param none
+             * @throws i2cSetupException
+             * @returns int deviceStatus : 1 = on, 0 = off or 2 = error
+             */
+            int start();
+
+            float pitch; //!< Stores the calculated Pitch value
+
+            float roll; //!< Stores the calculated Roll value
+
+            pthread_t threadHandle; //!< @var Thread Handle
 
         private:
 
-            void startRecordingPitchAndRoll();
+            /*!
+             * runMainSensorUpdateThread starts a continuous thread updating the programs values from the sensors values.
+             *
+             * @see start() to start this thread running
+             * @see stop() to stop this thread running
+             * @param static_inst
+             * @return 0
+             */
+            static void* runMainSensorUpdateThread( void* static_inst );
 
-            static void* getAccelerometerValues( void* _ACCEL );
 
-            pthread_t updateValuesThread;
+            int threadRet; //!< @var thread return value
         };
 
     }
