353d2b0aca76eeeb994c0fef106ef98bf5519569
Michael Brookes <mbircohoakeels@me.com>
2017-10-16 14:28:24 +0200

    Fixing case on include paths

--- lib/overlays/pinOverlay.cpp
+++ lib/overlays/pinOverlay.cpp
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "pinOverlay.h"
-#include "../PWM/pwmDevice.h"
+#include "../pwm/pwmDevice.h"
 
 using namespace quadro::overlays;
 
